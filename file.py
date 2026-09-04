dc6b311a1442386a1006a3adeb53f4c9e8428c37
lilola3 <lynnwhajj2002@gmail.com>
2026-09-04 11:58:55 +0300

    Fix smoothie response display in file.py

--- file.py
+++ file.py
@@ -54,6 +54,6 @@ if ingredients_list:
 
 import requests  
 smoothiefroot_response = requests.get("[https://my.smoothiefroot.com/api/fruit/watermelon](https://my.smoothiefroot.com/api/fruit/watermelon)")  
-st.text(.json)
+st.text(smoothiefroot_response.json())
 
 
